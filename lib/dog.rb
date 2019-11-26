4b46f77d5f421769b4c332154cdc38ba8240ecfb
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-26 21:38:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Dog 
+
+ def name=(d_name)
+   @dogo_name = d_name
+ end 
+ 
+ def bark 
+   puts "woof!"
+ end 
+   
+end
+
+
+dog_1 = Dog.new
+dog_1.name = "Fido"
+ 
